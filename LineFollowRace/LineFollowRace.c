f857676ec607f636255189fbcd635fecc39b40e1
ericsherman4 <esherman2018@gmail.com>
2023-02-28 09:39:28 -0500

    some work on alterate state machine

--- LineFollowRace/LineFollowRace.c
+++ LineFollowRace/LineFollowRace.c
@@ -21,10 +21,17 @@
 0   0,0     neither button      means lost
  */
 
+#define MOTOR_FORWARD 1
+#define MOTOR_LEFT 2
+#define MOTOR_RIGHT 3
+#define MOTOR_BACKWARD 4
+
 // Linked data structure
 struct State {
+
   uint16_t right_PWM;           // Right wheel PWM
   uint16_t left_PWM;            // Left wheel PWM
+  uint8_t function;            // motor function to call
   const struct State *next[64]; // Next if 6-bit input is 0-63
 };
 
@@ -41,20 +48,23 @@ typedef const struct State State_t;
 #define Error  &fsm[8]
 
 State_t fsm[9]={
-  {3000, 3000,  {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Center
-  {2000, 3000,  {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Left1
-  {1500, 3000,  {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Left2
-  {   0, 3000,  {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Left3
+  {4000, 4000, MOTOR_FORWARD , {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Center
+  {2000, 3000, MOTOR_FORWARD , {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Left1
+  {1500, 3000, MOTOR_FORWARD , {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Left2
+  {1500, 1500, MOTOR_LEFT , {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Left3
 
-  {3000, 2000,  {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Right1
-  {3000, 1500,  {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Right2
-  {3000,    0,  {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Right3
+  {3000, 2000, MOTOR_FORWARD , {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Right1
+  {3000, 1500, MOTOR_FORWARD , {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Right2
+  {1500, 1500, MOTOR_RIGHT , {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Right3
 
-  {   0,    0,  {Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,   Stop,  Stop,  Stop,  Stop,   Stop,   Stop,   Stop,   Stop,   Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,   Stop,  Stop,  Stop,  Stop,   Stop,   Stop,   Stop,   Stop,   Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,   Stop,  Stop,  Stop,  Stop,   Stop,   Stop,   Stop,   Stop,   Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,   Stop,  Stop,  Stop,  Stop,   Stop,   Stop,   Stop}},   // Stop
-  {   0,    0,  {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Error
+  {   0,    0, MOTOR_FORWARD , {Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,   Stop,  Stop,  Stop,  Stop,   Stop,   Stop,   Stop,   Stop,   Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,   Stop,  Stop,  Stop,  Stop,   Stop,   Stop,   Stop,   Stop,   Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,   Stop,  Stop,  Stop,  Stop,   Stop,   Stop,   Stop,   Stop,   Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,  Stop,   Stop,  Stop,  Stop,  Stop,   Stop,   Stop,   Stop}},   // Stop
+  { 2000,  2000, MOTOR_BACKWARD , {Error, Left3, Left2, Left2, Left1, Left1, Left1, Left1, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right3, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Center, Right2, Error, Error, Error, Error, Error, Error, Error, Right1, Error, Error, Error, Center, Center, Center, Error}}, // Error
 };
 
 State_t *Spt;  // pointer to the current state
+
+State_t *prevstates[4] = {Center, Center, Center, Center};
+
 
 
 /*Run FSM continuously
@@ -97,10 +107,46 @@ int main(void){
   while(1){
       if (!bump_detected)
       {
-          Motor_Forward(Spt->left_PWM, Spt->right_PWM);     // do output to two motors
+          //Motor_Forward(Spt->left_PWM, Spt->right_PWM);     // do output to two motors
+          switch(Spt->function)
+          {
+            case MOTOR_FORWARD:
+                Motor_Forward(Spt->left_PWM, Spt->right_PWM);
+                break;
+            case MOTOR_RIGHT:
+                Motor_Right(Spt->left_PWM, Spt->right_PWM);
+                break;
+            case MOTOR_LEFT:
+                Motor_Left(Spt->left_PWM, Spt->right_PWM);
+                break;
+            case MOTOR_BACKWARD:
+                Motor_Backward(Spt->left_PWM, Spt->right_PWM);
+                break;
+          }
+
           SysTick_Wait10ms(1);
           uint8_t Input = read();    // read sensors
+
           Spt = Spt->next[Input];       // next depends on input and state
+
+          // shift all of the previous states
+          prevstates[3] = prevstates[2];
+          prevstates[2] = prevstates[1];
+          prevstates[1] = prevstates[0];
+          prevstates[0] = Spt;
+
+          uint8_t count,i;
+          for(i = 0; i < 4; i++)
+          {
+            if(prevstates[i] == Error) count++;
+          }
+
+          if(count >= 3)
+          {
+            Motor_Forward(6000,6000);
+            SysTick_Wait10ms(20);
+          }
+
       }
   }
 }
